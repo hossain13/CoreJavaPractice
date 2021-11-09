16ab389ba097586b86a1505caa9ed5dfef3f84cd
Hossain <hossain13.ea@gmail.com>
2021-11-08 23:11:51 -0500

    Test

---
+++ src/tree/ForLoop.java
@@ -0,0 +1,40 @@
+//package tree;
+//
+//public class ForLoop {
+//
+//	new Tree(1);
+//	new Tree(2);
+//	new Tree(13);
+//	new Tree(14);
+//	Trees [] t = {new Tree(1),
+//	new Tree(2),
+//	new Tree(13),
+//	new Tree(14)}
+//	
+//	
+//
+//	
+//}
+//
+//
+//
+//class Tree {
+//	
+//	int a = 0;
+//	
+//	Tree(int a) {
+//		this.a = a;
+//	}
+//
+//	public int getA() {
+//		return a;
+//	}
+//
+//	public void setA(int a) {
+//		this.a = a;
+//	}
+//}
+//
+//class Camera{
+//	
+//}
